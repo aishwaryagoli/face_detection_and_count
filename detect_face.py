7047f1cf719b5fda98d42926af329b88dd628645
aishwaryagoli <34357299+aishwaryagoli@users.noreply.github.com>
2017-12-09 01:04:38 +0530

    Update detect_face.py

--- detect_face.py
+++ detect_face.py
@@ -1,40 +1,40 @@
+from firebase import firebase
 import numpy as np
 import cv2
-from firebase import firebase
 
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
 
-#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
 cap = cv2.VideoCapture(0)
+count=0
+i=1
 
 while 1:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    count=0
     
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        count=count+1
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-            
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
-        print("count=" ,count)
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
+count=len(faces)
 firebase = firebase.FirebaseApplication('https://sd14cs006.firebaseio.com/')
 data = {'faces': count}
-result=firebase.put('https://sd14cs006.firebaseio.com/','/facess',data)
+
+result= firebase.put('https://sd14cs006.firebaseio.com/','/face',data)
+
+
+print(len(faces))
